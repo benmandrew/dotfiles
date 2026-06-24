02520c1f7f84ccf286a415feb5658fb9b83f1904
benmandrew <benmandrew@gmail.com>
2026-06-24 19:45:08 +0100

    feat(wezterm): disable ligatures via harfbuzz_features

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local config = wezterm.config_builder()
 -- Font
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 13.0
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 -- Colors
 config.color_scheme = "Hacktober"
