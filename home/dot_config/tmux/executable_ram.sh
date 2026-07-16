13e8487657692b3ae0fc8b48c6dc1834a68737c7
benmandrew <benmandrew@gmail.com>
2026-07-16 14:06:32 +0100

    fix(tmux): keep the RAM readout width stable past 100GB
    Widen the printf field from %4.1f to %5.1f. Above 100GB the four-wide
    field overflowed to five characters, so the readout shifted as used memory
    crossed 99.9 to 100.0. Five characters hold up to 999.9G.

--- home/dot_config/tmux/executable_ram.sh
+++ home/dot_config/tmux/executable_ram.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 awk '/^MemTotal/{t=$2} /^MemAvailable/{a=$2} END{
-    printf "%4.1fG/%4.1fG\n", (t-a)/1048576, t/1048576
+    printf "%5.1fG/%5.1fG\n", (t-a)/1048576, t/1048576
 }' /proc/meminfo
